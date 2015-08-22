1e169dfb8bd564c3a02494adfccc1e48e3d09f24
Jessamyn Smith <geekchick77@gmail.com>
2015-08-22 00:40:44 -0400

    Try to fix heroku errors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static('app'));
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Authorization");
   next();
 });
 
